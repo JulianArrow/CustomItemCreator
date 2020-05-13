0d6b01af2261d54e3b60412f3c091a74493321ac
TideWoW <info@faux-wow.com>
2020-05-13 22:08:54 +0200

    Edited faq

--- tpl/faq.tpl.php
+++ tpl/faq.tpl.php
@@ -5,23 +5,21 @@ In order to have a smooth experience please follow the guidelines below.<br>
  
 <span style="font-size: 24px; font-weight:bold;">Creating a custom weapon</span><br>
 <ul>
-  <li>1. Please be sure that you're <a href="https://havoc-wow.com/login" target="_blank">logged in</a> on the website.</li>
-  <li>2. Start of by buying a VIP item from the <a href="https://havoc-wow.com/store" target="_blank">item store.</a></li>
-  <li>3. Head onto the item creator tab, select the VIP item you bought and start adding your stats.</li>
-  <li>4. After you're item has been successfully created, please send us a message on our <a href="https://discord.gg/HN928bb" target="_blank">Discord server</a> in the <span style="font-size: 18px; font-weight:bold; color:#81937f">#custom-item-request</span> channel saying "I need a reload".</li>
+	<li>1. Please be sure that you're <a href="https://havoc-wow.com/login" target="_blank">logged in</a> on the website.</li>
+	<li>2. Start of by buying a VIP item from the <a href="https://havoc-wow.com/store" target="_blank">item store.</a></li>
+	<li>3. Head onto the item creator tab, select the VIP item you bought and start adding your stats.</li>
 </ul> 
 	
 <span style="font-size: 24px; font-weight:bold;">Editing a custom weapon</span><br>
 <ul>
-  <li>1. Have a VIP item.</li>
-	<li>2. <a href="https://havoc-wow.com/custom/index.php?page=edit">Edit it</a></li>
-  <li>3. After you're item has been successfully edit, please send us a message on our <a href="https://discord.gg/HN928bb" target="_blank">Discord server</a> in the <span style="font-size: 18px; font-weight:bold; color:#81937f">#custom-item-request</span> channel saying "I need a reload".</li>
+	<li>1. Have a VIP item.</li>
+	<li>2. <a href="https://havoc-wow.com/itemcreator/index.php?page=edit">Edit it</a></li>
 </ul> 
 
 <span style="font-size: 24px; font-weight:bold;">Other notes:</span><br>
 <ul>
-  <li>Maximum 10 stats.</li>
-  <li>You can only edit your custom items.</li>
-  <li>ALWAYS carry your custom item in your backpack or equipped.</li>
-  <li>This page will be updated daily in order to achieve the product you and we all want. </li>
+	<li>Maximum 10 stats.</li>
+	<li>You can only edit your custom items.</li>
+	<li>ALWAYS carry your custom item in your backpack or equipped.</li>
+	<li>This page will be updated daily in order to achieve the product you and we all want. </li>
 </ul> 
