a7de6e983203a36b96fc6cc69ee9427e18492fc6
TideWoW <info@faux-wow.com>
2020-05-14 21:41:09 +0200

    Change name-format

--- const.inc.php
+++ const.inc.php
@@ -147,7 +147,7 @@ const colors = [
 ];
 
 const cItemsColumns = 'entry, class, subclass, name, displayid, InventoryType, StatsCount, stat_type1, stat_value1, stat_type2, stat_value2, stat_type3, stat_value3, stat_type4, stat_value4, stat_type5, stat_value5, stat_type6, stat_value6, stat_type7, stat_value7, stat_type8, stat_value8, stat_type9, stat_value9, stat_type10, stat_value10, dmg_min1, dmg_max1, delay, sheath, socketColor_1, socketColor_2, socketColor_3, Flags, quality, holy_res, fire_res, nature_res, arcane_res, frost_res, shadow_res, armor';
-const cItemsWhereClause = '(`name` LIKE "VIP _ %" OR `name` LIKE "VIP _.5 %" OR `name` LIKE "Donor %" OR `name` LIKE "*VIP _ %") AND `name` NOT LIKE "VIP _ Haste GEM" and `name` NOT LIKE "V__ 9 %"';
+const cItemsWhereClause = '(`name` LIKE "VIP _ %" OR `name` LIKE "VIP _.5 %" OR `name` LIKE "Donor %" OR `name` LIKE "|cffff0000*VIP _ %") AND `name` NOT LIKE "VIP _ Haste GEM" and `name` NOT LIKE "V__ 9 %"';
 
 const cItemTable = [
 	'tableName' => 'item_battleground_lock',
